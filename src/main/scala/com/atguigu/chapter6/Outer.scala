e305e7d36182e76eba86ed725e95a0451326848c
cathree <1570878357@qq.com>
2020-11-14 10:37:06 +0800

    项目上传

--- src/main/scala/com/atguigu/chapter6/Outer.scala
+++
@@ -1,8 +0,0 @@
-package com.atguigu.chapter6
-
-
-object Outer {
-  def main(args: Array[String]): Unit = {
-    val name:String = "xiaoming"
-  }
-}
--- src/main/scala/com/atguigu/scala/test/Test3.scala
+++
@@ -1,9 +0,0 @@
-package com.atguigu.scala.test
-
-object Test3 {
-  def main(args: Array[String]): Unit = {
-
-  }
-}
-
-
